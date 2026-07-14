60254e513e195cf1ba1778a60cfff00c8a24ecf7
Md Raza <mdraza8397@gmail.com>
2026-07-14 11:51:51 +0530

    fix https assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,31 +4,30 @@ namespace App\Providers;
 
 use App\Models\CompanySetting;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use \Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         try {
             if (Schema::hasTable('company_settings')) {
                 $globalSetting = CompanySetting::first();
                 View::share('globalSetting', $globalSetting);
             }
         } catch (\Exception $e) {
-            // Ignore during build/deployment
+            //
         }
     }
 }
